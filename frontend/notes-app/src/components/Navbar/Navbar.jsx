0ee2796307001851cf7676687ecf13c22a64c3c8
Hariharan2716 <hariharan.m2716@gmail.com>
2025-07-20 15:32:04 +0530

    Monorepo setup: frontend and backend organized

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ProfileInfo from "../Cards/Profileinfo";
+import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
