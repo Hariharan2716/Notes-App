941d9c3135fc8ea408a3ddb46cbab8606fcab0cb
Hariharan2716 <hariharan.m2716@gmail.com>
2025-07-21 18:57:45 +0530

    Added Pin Notes API

--- backend/index.js
+++ backend/index.js
@@ -237,7 +237,35 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
   }
 })
 
-// 
+// Update isPinned Value
+app.put("/update-note-pinned/:noteId", authenticateToken, async(req, res) => {
+  const noteId = req.params.noteId;
+  const { isPinned } = req.body;
+  const { user } = req.user;
+
+  try {
+    const note = await Note.findOne({_id: noteId, userId: user._id});
+
+    if(!note) {
+      return res.status(404).json({error: true, message: "Note not found"});
+    }
+
+    note.isPinned = isPinned || false;
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note updated successfully",
+    });
+  } catch (error) {
+    return res.status(50).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+})
 app.listen(8000);
 module.exports = app;
 
